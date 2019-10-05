d009641b568840b72297c2d7ced5c93e112819c4
Peter Steinkamp <peter.steinkamp@googlemail.com>
2019-10-05 13:40:33 +0200

    Generator Bugfix

---
+++ ParallelRunner/Assets/Scripts/MapGen.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGen : MonoBehaviour
+{
+    public GameObject[] emptyTiles;
+    public GameObject[] blockedTiles;
+    public GameObject[] otherTiles;
+    public Transform Container;
+    
+    private void Start()
+    {
+        
+    }
+}
